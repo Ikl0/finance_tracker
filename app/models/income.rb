f7f199580fe031932b4e8f3a52b9fc9d7f3f0513
ikl0 <klimovs2008van@gmail.com>
2024-06-18 02:16:19 +0300

    comment unused

--- app/models/income.rb
+++ app/models/income.rb
@@ -3,9 +3,9 @@
 class Income < ApplicationRecord
   belongs_to :user
   validates :name, length: { minimum: 3, maximum: 50 }, allow_blank: false
-  self.primary_key = 'id'
-  validates :name, length: { minimum: 3, maximum: 50 }, allow_blank: false
+  #self.primary_key = 'id'
   has_many :operation_details, dependent: :delete_all
+
   scope :with_user, ->(user_id) { where('user_id = ?', user_id) }
   scope :with_family, lambda { |family_id|
     where('user_id IN (?)', User.with_family(family_id).pluck(:id))
