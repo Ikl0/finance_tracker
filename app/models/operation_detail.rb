22a59c2c9b126cb923bff400ffbfed0cbadca25d
ikl0 <klimovs2008van@gmail.com>
2024-06-18 02:15:43 +0300

    remove unused block

--- app/models/operation_detail.rb
+++ app/models/operation_detail.rb
@@ -4,7 +4,7 @@ class OperationDetail < ApplicationRecord
   validates :operation_id, presence: true
   validates_numericality_of :amount, greater_than: 0.0
   belongs_to :operation
-  validate :any_present?
+  #validate :any_present?
 
   def any_present?
     return if %w[income_id expense_id].none? { |attr| self[attr].present? }
