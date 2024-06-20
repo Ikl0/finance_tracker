4036def57a6dfd49cd2637c0bf6223f8c72c994c
ikl0 <klimovs2008van@gmail.com>
2024-06-21 00:25:37 +0300

    try use optional setting

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:drop
-bundle exec rails db:setup
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:setup
